23ab670fd183f66149b002a2aa06afa654e7dd21
sugeesh <lpdneo@gmail.com>
2015-12-27 17:36:45 +0530

    userController updated

--- php/mysql_connector.php
+++ php/mysql_connector.php
@@ -1,15 +1,20 @@
 <?php
 
-$db_host = "localhost";
-$db_username="root";
-$db_password="lpdscneo";
-$db_name="pharmacydb";
+function getConnection()
+{
+    $db_host = "localhost";
+    $db_username = "root";
+    $db_password = "lpdscneo";
+    $db_name = "pharmacydb";
 
-$connection = mysqli_connect("$db_host","$db_username","$db_password","$db_name");
+    $connection = mysqli_connect("$db_host", "$db_username", "$db_password", "$db_name");
 
 // Check connection
-if (mysqli_connect_errno())
-{
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+    return $connection;
 }
-?> 
+
+?>
--- register.php
+++ register.php
@@ -66,6 +66,7 @@
                             <div class="control-group">
                                 <label class="control-label">User Name</label>
                                 <div class="controls">
+                                    <input type="text" placeholder="Enter your username" id="username" name ="username" class="input-xlarge">
                                     <input type="text" placeholder="Enter your username" id="username" class="input-xlarge">
                                 </div>
                             </div>
@@ -73,6 +74,7 @@
                             <div class="control-group">
                                 <label class="control-label">Password</label>
                                 <div class="controls">
+                                    <input type="password" placeholder="Enter your password" id="password" name="password"
                                     <input type="password" placeholder="Enter your password" id="password"
                                            class="input-xlarge">
                                 </div>
