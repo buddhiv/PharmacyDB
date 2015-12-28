4c75926c4bce6863906c973c5f6aa8f382ffe0c8
sugeesh <lpdneo@gmail.com>
2015-12-28 14:03:03 +0530

    LogIn Completed

--- php/controller/userController.php
+++ php/controller/userController.php
@@ -10,12 +10,11 @@ include_once "../mysql_connector.php";
 
 if (isset($_POST["addUser"])) {
     addUser();
-
+}elseif(isset($_POST["login"])) {
+    logIn();
 }
 
-function addUser()
-{
-
+function addUser(){
     $userName = preg_replace('#[^A-Za-z0-9]#', '', $_POST["username"]);
     $fullName = preg_replace('#[^A-Za-z0-9]#', '', $_POST["fullname"]);
     $address = preg_replace('#[^A-Za-z0-9]#', '', $_POST["address"]);
@@ -38,6 +37,32 @@ function addUser()
     $connection->close();
 
     header('Location: http://localhost/PharmacyDB/login.php');
+}
+
+function logIn(){
+    session_start();
+    $userName = preg_replace('#[^A-Za-z0-9]#', '', $_POST["username"]);
+    $password = preg_replace('#[^A-Za-z0-9]#', '', $_POST["password"]);
+
+    $connection = getConnection();
+    $sql = mysqli_query($connection,"SELECT CustomerId FROM fcustomer WHERE username='$userName' AND password='$password' LIMIT 1");
+
+    $existCount = mysqli_num_rows($sql);
+    if ($existCount == 1) {
+        while ($row = mysqli_fetch_array($sql)) {
+            $id = $row["CustomerId"];
+        }
+        $_SESSION["customerId"] = $id;
+        $_SESSION["customerName"] = $userName;
+        $_SESSION["password"] = $password;
+        header('Location: http://localhost/PharmacyDB/index.php');
+    } else {
+        echo "The information incorrect. Try again.";
+        header('Location: http://localhost/PharmacyDB/login.php');
+
+    }
+    $connection->close();
+
 }
 
 
